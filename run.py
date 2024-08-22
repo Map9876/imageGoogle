bb4e7670e5b948be910e19ba127b56146de0f173
Map987 <99967856+Map987@users.noreply.github.com>
2024-08-22 11:01:34 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+# 确保您的ChromeDriver路径是正确的
+service = Service('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=service)
 driver.get("https://www.google.com")
+print(f"ok")
 driver.quit()
-
-print('i am fine!')
-driver.close() 
