abd23e96e5c24dab0b8bbf3faa5cd69938c45b48
Map987 <99967856+Map987@users.noreply.github.com>
2024-08-22 11:36:44 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
+service = Service()
 options.add_argument("webdriver.chrome.driver=/usr/local/bin/chromedriver")
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
