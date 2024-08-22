84f1d024e257c72e74b17c520b9ab53052208a1a
Map987 <99967856+Map987@users.noreply.github.com>
2024-08-22 11:34:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+options.add_argument("webdriver.chrome.driver=/usr/local/bin/chromedriver")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(service=service,  options=options)
 
-# 确保您的ChromeDriver路径是正确的
-driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get("https://www.google.com")
 print(f"ok")
 driver.quit()
