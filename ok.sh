3a9f5b38da149097cfd43ed52dd338efce72b3cb
Map987 <99967856+Map987@users.noreply.github.com>
2024-09-17 21:38:05 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -3,3 +3,8 @@ URL=$1
 curl -s "$URL" | exiftool -fast -
 ' > /data/data/com.termux/ok.sh
 chmod +x /data/data/com.termux/ok.sh
+
+
+echo 'alias e='data/data/com.termux/ok/ok.sh'
+' > ~/.bashrc
+source ~/.bashrc
