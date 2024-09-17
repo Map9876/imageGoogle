17e909cead547d0354980f96de95a598a52ddfc1
Map987 <99967856+Map987@users.noreply.github.com>
2024-09-17 22:02:05 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -2,10 +2,9 @@ echo '#!/bin/bash
 URL=$1
 
 curl -s "$URL" | exiftool -fast -
-' > /data/data/com.termux/ok.sh
+' > /data/data/com.termux/ok.sh # e命令执行的命令可自定义
 chmod +x /data/data/com.termux/ok.sh
 
 
-echo "alias e='/data/data/com.termux/ok/ok.sh'
-" > ~/.bashrc
+echo "alias e='/data/data/com.termux/ok/ok.sh'" >> ~/.bashrc # e可自定义
 source ~/.bashrc
